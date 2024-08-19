33820d4927497fbc98ea39334108fca8b27d1881
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-08-19 23:24:24 +0800

    Update Triangle.java

--- Chapter 1/Triangle.java
+++ Chapter 1/Triangle.java
@@ -1,41 +1,36 @@
 // Chapter 1 ( Triangle.java )
 // French Cyril Sambilad BSIT-2E
 
-import java.util.Scanner;
-
 public class Triangle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int rows = 6; 
 
-        System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-    
         for (int i = 1; i <= rows; i++) {
-          
+
             for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
-            // Print 'T' with spaces between them
-            for (int j = 1; j <= i; j++) {
-                System.out.print("T ");
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("T");
             }
-           
+
             System.out.println();
         }
-
     }
 }
 /*
-public class PyramidT {
+ public class Triangle {
     public static void main(String[] args) {
         // Print each line of the pyramid
         System.out.println("      T      ");
-        System.out.println("     T T     ");
-        System.out.println("    T T T    ");
-        System.out.println("   T T T T   ");
-        System.out.println("  T T T T T  ");
-        System.out.println(" T T T T T T ");
-        System.out.println("T T T T T T T");
+        System.out.println("     TTT     ");
+        System.out.println("    TTTTT    ");
+        System.out.println("   TTTTTTT   ");
+        System.out.println("  TTTTTTTTT  ");
+        System.out.println(" TTTTTTTTTTT ");
+        System.out.println("TTTTTTTTTTTTT");
     }
-} */
+}
+*/
 
