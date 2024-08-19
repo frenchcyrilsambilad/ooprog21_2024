77d9a23252c170e8e46e7d5a88e5de6fe1bc8274
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-08-19 23:31:35 +0800

    Update MovieQuoteInfo.java

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -1,10 +1,8 @@
 // Chapter 1 ( MovieQuoteInfo.java )
 // French Cyril Sambilad BSIT-2E
 
-public class MovieQuoteInfo
-{
-   public static void main(String[] args)
-   {
+public class MovieQuoteInfo {
+   public static void main(String[] args) {
       System.out.println("Quote:I am Iron Man.");
       System.out.println("Movie:Iron Man");
       System.out.println("Actor:Robert Downey Jr.");
