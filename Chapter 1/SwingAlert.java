c7312c57715c0d90ae9e686559db66399aa47a25
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-08-21 21:04:59 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert{
+   public static void main(String[] args){
+      JOptionPane.showMessageDialog(null, "Hello World!");
+   }
+}
+
+/*
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+        int numberToGuess = 5; 
+        String guess = JOptionPane.showInputDialog("Guess a number between 1 and 10:");
+        int guessedNumber = Integer.parseInt(guess);
+
+        if (guessedNumber == numberToGuess) {
+            JOptionPane.showMessageDialog(null, "You guessed it! The number is 5.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Oops! Wrong guess. The number was 5.");
+        }
+
+        JOptionPane.showMessageDialog(null, "Thanks for playing!");
+    }
+}
+*/
