13d34a4471e00d616eefc572b719f8bedd219fd9
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-09-16 19:42:56 +0800

    Update FirstAddressExcercise.java

--- Chapter 3/FirstAddressExcercise.java
+++ Chapter 3/FirstAddressExcercise.java
@@ -2,8 +2,9 @@ class FirstAddressExercise {
     public static void displayAddress() {
         System.out.println("Xiaomi Corporation");
         System.out.println("No. 68, Qinghe Middle Street");
-        System.out.println("Haidian District, Beijing");
+        System.out.println("Haidian District, Beijing 100085");
         System.out.println("China");
+        System.out.println("Company: Xiaomi Corporation");
     }
 
     public static void main(String[] args) {
