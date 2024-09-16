b858ae548adcd59426964a04e2b13d42e9e4fe5a
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-09-16 19:38:33 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,12 @@
+class FirstAddressExercise {
+    public static void displayAddress() {
+        System.out.println("Xiaomi Corporation");
+        System.out.println("No. 68, Qinghe Middle Street");
+        System.out.println("Haidian District, Beijing");
+        System.out.println("China");
+    }
+
+    public static void main(String[] args) {
+        displayAddress();
+    }
+}
