88292af31925dac9a89fd0e9130d614e67e84387
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-10-23 22:50:25 +0800

    Update SortThreeNumbers.java

--- Chapter 5/SortThreeNumbers.java
+++ Chapter 5/SortThreeNumbers.java
@@ -4,7 +4,7 @@ public class SortThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Please input three numbers. ");
+        System.out.println("Please input three numbers.");
         
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
@@ -15,24 +15,19 @@ public class SortThreeNumbers {
         System.out.print("Enter third number: ");
         int num3 = scanner.nextInt();
         
-        
+        System.out.print("Numbers rearranged in ascending order: ");
         if (num1 <= num2 && num2 <= num3) {
-            System.out.println("Numbers rearranged in ascending order: " + num1 + ", " + num2 + ", " + num3);
+            System.out.println(num1 + " " + num2 + " " + num3);
+        } else if (num1 <= num3 && num3 <= num2) {
+            System.out.println(num1 + " " + num3 + " " + num2);
+        } else if (num2 <= num1 && num1 <= num3) {
+            System.out.println(num2 + " " + num1 + " " + num3);
+        } else if (num2 <= num3 && num3 <= num1) {
+            System.out.println(num2 + " " + num3 + " " + num1);
+        } else if (num3 <= num1 && num1 <= num2) {
+            System.out.println(num3 + " " + num1 + " " + num2);
         } else {
-            
-            if (num1 >= num2 && num2 >= num3) {
-                System.out.println("Numbers rearranged in descending order: " + num1 + ", " + num2 + ", " + num3);
-            } else if (num1 >= num3 && num3 >= num2) {
-                System.out.println("Numbers rearranged in descending order: " + num1 + ", " + num3 + ", " + num2);
-            } else if (num2 >= num1 && num1 >= num3) {
-                System.out.println("Numbers rearranged in descending order: " + num2 + ", " + num1 + ", " + num3);
-            } else if (num2 >= num3 && num3 >= num1) {
-                System.out.println("Numbers rearranged in descending order: " + num2 + ", " + num3 + ", " + num1);
-            } else if (num3 >= num1 && num1 >= num2) {
-                System.out.println("Numbers rearranged in descending order: " + num3 + ", " + num1 + ", " + num2);
-            } else {
-                System.out.println("Numbers rearranged in descending order: " + num3 + ", " + num2 + ", " + num1);
-            }
+            System.out.println(num3 + " " + num2 + " " + num1);
         }
     }
 }
