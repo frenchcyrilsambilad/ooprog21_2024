9d430e00d099c9448dcab4d2c3a11fe0fca2ae90
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2024-10-23 22:47:49 +0800

    Update SortThreeNumbers.java

--- Chapter 5/SortThreeNumbers.java
+++ Chapter 5/SortThreeNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SortNumbers {
+public class SortThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
